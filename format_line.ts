9e0a743f56ada1a0ae88ed09070e9f7464a12119
Ayush Pandey <ayushpandey10851@gmail.com>
2026-08-19 18:51:20 +0530

    fix: resolve format_line type safety issue

--- format_line.ts
+++ format_line.ts
@@ -1,12 +1,7 @@
-diff --git a/format_line.ts b/format_line.ts
-index 1a2b3c4..5d6e7f8 100644
---- a/format_line.ts
-@@ -10,7 +10,7 @@ function formatLine(input: string): string {
-   return input.replace(/\s+/g, ' ');
- }
- 
--// @ts-ignore
-+// TODO: Resolve type safety issue
- function transformLine(input: any) {
-   return formatLine(input);
- }
+function formatLine(input: string): string {
+  return input.replace(/\s+/g, ' ');
+}
+
+function transformLine(input: string) {
+  return formatLine(input);
+}
